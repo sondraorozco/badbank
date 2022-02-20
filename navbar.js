4d33dcbdece5fdf49feae73eb56dfd477f2d3062
Sondra <sondra.suazo@gmail.com>
2022-02-20 10:03:05 -0800

    Update navbar.js to use NavBar React Router component and style link to active page

--- navbar.js
+++ navbar.js
@@ -1,40 +1,37 @@
 function NavBar() {
 
-  function classes() {
-    // const isActive = ' active'; // how to set this??
-    return 'nav-link' 
-  }
-
-  function classes() {
-    const bg = props.bgcolor ? ' bg-' + props.bgcolor : ' ';
-    const txt = props.txtcolor ? ' text-' + props.txtcolor : ' ';
-    return 'card mb-3 ' + bg + txt;
-  }
+  const activeStyle = {
+    fontWeight: "bold", 
+    color: "MediumSeaGreen",
+    textDecoration: "underline"
+  };
 
   return (
-    
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
         <div className="container container-fluid">
-          <a className="navbar-brand" href="#">BadBank</a>
+          <a className="navbar-brand" href="/">BadBank</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#/CreateAccount">Create Account</a>
+            <li className="nav-item">
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/" exact={true}>Home</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#/login">Login</a>
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/CreateAccount">Create Account</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#/deposit">Deposit</a>
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/login">Login</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#/withdraw">Withdraw</a>
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/deposit">Deposit</NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#/alldata">AllData</a>
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/withdraw">Withdraw</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" activeStyle={activeStyle} to="/alldata">AllData</NavLink>
               </li>
             </ul>
           </div>
