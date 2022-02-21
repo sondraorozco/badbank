362e75691fb4374a555930c6efe53ca3661d252d
Sondra <sondra.suazo@gmail.com>
2022-02-20 19:25:05 -0800

    Add BankForm component (has bugs, still work in progress)

--- context.js
+++ context.js
@@ -1,8 +1,9 @@
-const Route       = ReactRouterDOM.Route;
-const Link        = ReactRouterDOM.Link;
-const NavLink     = ReactRouterDOM.NavLink;
-const HashRouter  = ReactRouterDOM.HashRouter;
-const UserContext = React.createContext(null);
+const Route               = ReactRouterDOM.Route;
+const Link                = ReactRouterDOM.Link;
+const NavLink             = ReactRouterDOM.NavLink;
+const HashRouter          = ReactRouterDOM.HashRouter;
+const UserContext         = React.createContext(null);
+const CurrentUserContext  = React.createContext(null);
 
 function Card(props) {
   function classes() {
@@ -24,6 +25,75 @@ function Card(props) {
       </div>
     </div>
   );
+};
 
-}
+function BankForm(props) {
+  const { initialValues, fields, title, handle, hideBalance, submitText, successMessage, successButton } = props;
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
+  const [formValues, setFormValues] = React.useState({...initialValues});
+  
+  const fieldList = fields.map((item, index) => {
+    const { label, type='text', id, name, placeholder='' } = item;
+    return (
+      <div className="mb-3" key={index}>
+        <label>{label}</label>
+        <input type={type} className="form-control" id={id} name={name} placeholder={placeholder} value={formValues[id]} onChange={handleFormChange} />
+      </div>
+    );
+  });
+  
+  const handleFormChange = (event) => {
+    const { name, value } = event.target;
+    const newValues = {
+      ...formValues,
+      [name]: value
+    };
+    console.log('Form changed: ', newValues);
+    setFormValues(newValues);
+  };
+
+  function clearForm() {
+    setFormValues({...initialValues});
+    setShow(true);
+  };
+
+  const handleFormSubmit = (formValues, callback) => {
+    if (callback(formValues)) {
+      setShow(false);
+    } else {
+      setTimeout(() => setStatus('error'),3000);
+      return false;
+    }
+  };
+
+  return (
+    <Card
+      header={title}
+      status={status}
+      body={show ? (
+          <form>
+            {fieldList}
+
+            <button 
+              type="submit" 
+              className="btn btn-light" 
+              onClick={() => {
+                console.log('Submitting deposit: ' + formValues.amount)
+                handleFormSubmit(formValues, handle);
+              }}
+            >
+              {submitText}
+            </button>
+          </form>
+        ) : (
+          <>
+            <h5>Success!</h5>
+            <p>{successMessage}</p>
+            <button type="submit" className="btn btn-light" onClick={clearForm}>{successButton}</button>
+          </>
+        )}
+    />
+  );
+};
 
--- deposit.js
+++ deposit.js
@@ -1,10 +1,38 @@
 function Deposit() {
   const ctx = React.useContext(UserContext);
+  const userId = React.useContext(CurrentUserContext);
+
+  const initialValues = {
+    amount: '',
+  };
+
+  let fields = [
+    {id:"amount", name:"amount", label:"Deposit Amount", type:"number" },
+  ];
+
+  function handleDeposit(values) {
+    if (values.amount > 0) {
+      ctx[0].balance = ctx[0].balance + values.amount;
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return (
     <>
-      <h1>Deposit</h1>
-      <p>{JSON.stringify(ctx)}</p>
+      <h1 className="mb-3">Account Deposit</h1>
+
+      <BankForm 
+        title="Deposit"
+        initialValue={initialValues}
+        fields={fields}
+        handle={handleDeposit}
+        hideBalance={false}
+        submitText="Deposit"
+        successMessage="The deposited funds have been added to your account."
+        successButton="Make another deposit"
+      />
     </>
   );
 }
