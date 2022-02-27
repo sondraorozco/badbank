62f4ddb49826ced243d73719957346d63a8a8f95
Sondra <sondra.suazo@gmail.com>
2022-02-26 20:15:17 -0800

    Update CreateAccount component to use BankForm component

--- createaccount.js
+++ createaccount.js
@@ -1,57 +1,70 @@
 function CreateAccount() {
-  const [show, setShow]         = React.useState(true);
-  const [status, setStatus]     = React.useState('');
-  const [name, setName]         = React.useState('');
-  const [email, setEmail]       = React.useState('');
-  const [password, setPassword] = React.useState('');
   const ctx = React.useContext(UserContext);
+  const userId = React.useContext(CurrentUserContext);
 
-  function validate(field, label){
-    if (!field) {
-      setStatus('Error: ' + label + ' is a required field');
-      setTimeout(() => setStatus(''),3000);
-      return false;
+  const initialValues = {
+    name: '',
+    balance: '',
+    email: '',
+    password: ''
+  };
+
+  let fields = [
+    {id:"name", name:"name", label:"Name", type:"text", placeholder: "Michael Scott" },
+    {id:"balance", name:"balance", label:"Initial Deposit", type:"text"},
+    {id:"email", name:"email", label:"Email", type:"email", placeholder: "michael@dundermifflin.net" },
+    {id:"password", name:"password", label:"Password", type:"password", placeholder: "Enter a password" },
+  ];
+
+  function handleCreateAccount(values) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    // validate form fields and store any errors
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Name is a required field"
+    };
+
+    if (!values.balance) {
+      errors.balance = "An initial deposit is required"
+    } else if (isNaN(values.balance)) {
+      errors.balance = "Amount must be a number"
+    } else if (values.balance < 100) {
+      errors.balance = "A minimum initial deposit of $100 is required"
+    };
+
+    if (!values.email) {
+      errors.email = "Email is a required field"
+    } else if (!regex.test(values.email)) {
+      errors.email = "Must be a valid email address"
+    };
+
+    if (!values.password) {
+      errors.password = "Password is a required field"
+    } else if (values.password.length < 8) {
+      errors.password = "Your password must be at least 8 characters"
+    };
+
+    // if no errors found, update user's account balance
+    if (Object.keys(errors).length === 0) {
+      ctx.users.push({...values});
     }
-    return true;
-  }
 
-  function handleCreate() {
-    console.log(name, email, password);
-    if (!validate(name,     'name'))     return;
-    if (!validate(email,    'email'))    return;
-    if (!validate(password, 'password')) return;
-    ctx.users.push({name, email, password, balance:100});
-    setShow(false);
-  }
-
-  function clearForm() {
-    setName('');
-    setEmail('');
-    setPassword('');
-    setShow(true);
-  }
+    return errors;
+  };
 
   return (
-    <Card
-      header="Create Account"
-      status={status}
-      body={show ? (
-          <>
-            Name<br/>
-            <input type="text" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
-            Email<br/>
-            <input type="email" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)} /><br/>
-            Password<br/>
-            <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br/>
-            <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
-          </>
-        ) : (
-          <>
-            <h5>Success!</h5>
-            <p>Your account has been created.</p>
-            <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
-          </>
-        )}
-    />
+    <>
+      <BankForm 
+        header="Create Account"
+        initialValues={initialValues}
+        fields={fields}
+        handle={handleCreateAccount}
+        showBalance={false}
+        submitText="Create account"
+        successMessage="Your account has been created."
+        successButton="Add another account"
+      />
+    </>
   );
 }
